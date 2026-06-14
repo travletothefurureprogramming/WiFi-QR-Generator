ea39b501b587a18df9865f6a0e6a0c419b70aa2c
Gregory Iosifidis <travletothefurureprogramming@gmail.com>
2026-06-14 15:02:23 +0300

    Enhance QR code generation with directory selection, print option, and password visibility toggle; add .gitignore for history file

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import customtkinter as ctk
+from tkinter import messagebox
+from tkinter import filedialog
 import qrcode
 import datetime
 from PIL import Image
+import win32api
+import win32print
+
+
+password_showed = False
 
 
 def generate_qr_code():
@@ -34,12 +41,16 @@ def generate_qr_code():
 
     datetime_now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
 
-    filename = f"wifi_qr_{datetime_now}.png"
+    directory_to_save = filedialog.askdirectory()
+
+    filename = f"{directory_to_save}/wifi_qr_{datetime_now}.png"
 
     img = qr.make_image(fill_color="black", back_color="white")
     img.save(filename)
 
     print(f"\n✓ QR code saved as {filename}")
+    messagebox.showinfo("QR code",f"✓ QR code saved as {filename}")
+    
 
     with open("history.txt", "a") as f:
         f.write(f"{ssid}\n")
@@ -47,17 +58,31 @@ def generate_qr_code():
     img = Image.open(filename)
     
     preview_img = ctk.CTkImage(
-    light_image=img,
-    dark_image=img,
-    size=(200, 200)
+        light_image=img,
+        dark_image=img,
+        size=(200, 200)
     )
 
     image_label.configure(image=preview_img)
     image_label.image = preview_img
 
+    if do_you_want_to_print_check_box:
+        win32api.ShellExecute(0, 'print', filename, f'/d:"{win32print.GetDefaultPrinter()}"', '.', 0)
+
+
+def toogle_show_password():
+   global password_showed
+
+   if password_showed:
+    password_entry.configure(show="*")
+    password_showed = False
+   else:
+    password_entry.configure(show="")
+    password_showed = True
+
 app = ctk.CTk()
 app.title("WiFi QR Generator")
-app.geometry("500x500")
+app.geometry("600x600")
 
 ssid_label = ctk.CTkLabel(app,text="SSID")
 ssid_label.pack(pady=2)
@@ -68,8 +93,11 @@ ssid_entry.pack(pady=2)
 password_label = ctk.CTkLabel(app,text="PASSWORD")
 password_label.pack(pady=2)
 
-password_entry = ctk.CTkEntry(app)
+password_entry = ctk.CTkEntry(app,show="*")
 password_entry.pack(pady=2)
+
+show_check_box = ctk.CTkCheckBox(app,text="Show Password",command=toogle_show_password)
+show_check_box.pack(pady=2)
 
 security_label = ctk.CTkLabel(app,text="SECURITY TYPE")
 security_label.pack(pady=2)
@@ -83,8 +111,11 @@ size_label.pack(pady=2)
 size_dropdown = ctk.CTkOptionMenu(app,values=["Small","Medium","Large"])
 size_dropdown.pack(pady=2)
 
+do_you_want_to_print_check_box = ctk.CTkCheckBox(app,text="Do you want to print the QR code")
+do_you_want_to_print_check_box.pack(pady=2)
+
 generate_btn = ctk.CTkButton(app,text="Generate",command=generate_qr_code)
-generate_btn.pack()
+generate_btn.pack(pady=2)
 
 image_label = ctk.CTkLabel(app, text="")
 image_label.pack(pady=10)
